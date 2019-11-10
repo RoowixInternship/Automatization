64e2c3d7c31c7b5ca59036f2d09592f3eacc36b7
Daria Glagol <daria.glagol@gmail.com>
2019-11-10 11:05:52 +0700

    changes

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("css", function () {
+    return gulp.src("source/scss/style.scss")
+        .pipe(plumber())
+        .pipe(sourcemap.init())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(sourcemap.write("."))
+        .pipe(gulp.dest("build/css"))
+        .pipe(server.stream());
+});
+
+gulp.task("server", function () {
+    server.init({
+        server: "build/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
+
+    gulp.watch("source/less/**/*.scss", gulp.series("css"));
+    gulp.watch("source/*.html").on("change", server.reload);
+});
+
+
+gulp.task("html", function () {
+    return gulp.src("source/*.html")
+        .pipe(gulp.dest("build"));
+});
+
+gulp.task("start", gulp.series("css", "html", "server"));
